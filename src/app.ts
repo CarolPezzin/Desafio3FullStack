9caef3c4fb7d28ce801c464100e87ef24d93e4b6
CarolPezzin <102972588+CarolPezzin@users.noreply.github.com>
2023-02-07 16:07:12 -0300

    add lib cors

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,10 @@ import "reflect-metadata";
 import express from "express";
 import userRoutes from "./routes/users.routes";
 import sessionsRoutes from "./routes/sessions.routes";
+import cors from "cors";
 
 const app = express()
-
+app.use(cors())
 app.use(express.json())
 app.use('/users', userRoutes)
 app.use('/login', sessionsRoutes)
